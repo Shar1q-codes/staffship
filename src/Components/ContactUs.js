84095710b0ac1720911c8d69276615572407d1ee
Shar1q-codes <shariqhussain691@gmail.com>
2022-10-26 14:58:54 +0530

    done

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { FaLinkedin } from 'react-icons/fa'
 const ContactUs = () => {
   return (
-    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium voluptates at, ipsam nemo ea modi nihil harum deserunt dicta necessitatibus error magni? Eos amet iure vel debitis labore dolore laborum placeat doloribus temporibus, quia sequi laudantium nesciunt, dolorum facilis tenetur officiis voluptatibus nobis a blanditiis quos repellat adipisci! Quia, modi?</p>
+    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className='contact-component'>
+      <div className="contact-container">
+
+        <h1 className='contact-info-heading'>Contact Us</h1>
+
+        <p className='contact-info'>Email : mohammed.khaleel@staffship.in</p>
+
+        <p className='contact-info'>Contact : India : +91 9700904807</p>
+
+        <p className='contact-info'>Contact : USA : +1 732 333 4566</p>
+      </div>
+
+      <div className="social-container">
+        <h1 className="social-info-head">Our Socials</h1>
+        <a href="https://www.linkedin.com/company/staff-ship/" target="_blank" rel='noopener noreferrer'>
+
+        <FaLinkedin className='linkedin'/>
+        </a>
+      </div>
     </motion.div>
   )
 }
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { motion } from 'framer-motion'
 const Services = () => {
   return (
-    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium voluptates at, ipsam nemo ea modi nihil harum deserunt dicta necessitatibus error magni? Eos amet iure vel debitis labore dolore laborum placeat doloribus temporibus, quia sequi laudantium nesciunt, dolorum facilis tenetur officiis voluptatibus nobis a blanditiis quos repellat adipisci! Quia, modi?</p>
+    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className='services-component'>
+      <p>Under Construction</p>
     </motion.div>
   )
 }
