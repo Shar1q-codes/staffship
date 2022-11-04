cbd0764ce8f3060c8d771289f1e065140c13a9b9
Shar1q-codes <shariqhussain691@gmail.com>
2022-11-05 01:29:28 +0530

    done

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import ServicesNav from './ServicesNav'
+
 
 const Services = () => {
   return (
     <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className='services-component'>
-      <ServicesNav/>
+     
       <div className='services-container'>
         <div id='global'>
           <h1>Global Recruiting</h1>
